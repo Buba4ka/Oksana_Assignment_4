d605c456852bb3ade6e011f5bf8d6e05c35d5779
Buba4ka <61472783+Buba4ka@users.noreply.github.com>
2022-04-25 21:57:06 -0400

    v3

--- src/com/coderscampus/service/CSVFileServiceImpl.java
+++ src/com/coderscampus/service/CSVFileServiceImpl.java
@@ -6,6 +6,7 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
 
+
 public class CSVFileServiceImpl implements CSVFileService {
 
 
@@ -53,103 +54,58 @@ public class CSVFileServiceImpl implements CSVFileService {
     @Override
     public void divideStudentsByCourse() throws IOException {
         Student[] allStudents = createStudents();
-        Student[] course1 = new Student[101];
-        Student[] course2 = new Student[101];
-        Student[] course3 = new Student[101];
 
-        BufferedWriter fileWriter = null;
-        BufferedWriter fileWriter2 = null;
-        BufferedWriter fileWriter3 = null;
+        BufferedWriter [] fileWriters = new BufferedWriter[3];
+        fileWriters[0] = new BufferedWriter(new FileWriter("course1.csv"));
+        fileWriters[1] = new BufferedWriter(new FileWriter("course2.csv"));
+        fileWriters[2] = new BufferedWriter(new FileWriter("course3.csv"));
 
-        //String studentInfo1 = "";
         Student headings = allStudents[0];
+        String headString = "";
         try {
-            fileWriter = new BufferedWriter(new FileWriter("course1.csv"));
-            fileWriter2 = new BufferedWriter(new FileWriter("course2.csv"));
-            fileWriter3 = new BufferedWriter(new FileWriter("course3.csv"));
-
-            //int index = 0;
-            int i1 = 0;
-            int i2 = 0;
-            int i3 = 0;
-
-            String stCourse1 = "";
-            String stCourse2 = "";
-            String stCourse3 = "";
-            String headString ="";
-
-
-            for (int index = 0; index < allStudents.length; index++) {
-                if (allStudents[index] == headings) {
-                    headString = allStudents[index].getStudentID() + "," + allStudents[index].getName() + ","
-                            + allStudents[index].getCourse() + "," + allStudents[index].getGrade();
-                    fileWriter.write(headString + "\n");
-                    fileWriter2.write(headString + "\n");
-                    fileWriter3.write(headString + "\n");
-                    course1[i1] = allStudents[index];
-                    course2[i2] = allStudents[index];
-                    course2[i3] = allStudents[index];
-                    i1++;
-                    i2++;
-                    i3++;
-                }else if (allStudents[index].getCourse().contains("COMPSCI")) {
-                    stCourse1 = allStudents[index].getStudentID() + "," + allStudents[index].getName() + ","
-                            + allStudents[index].getCourse() + "," + allStudents[index].getGrade();
-
-                    fileWriter.write(stCourse1 + "\n");
-                    course1[i1] = allStudents[index];
-                    i1++;
-                } else if (allStudents[index].getCourse().contains("STAT")) {
-                    stCourse2 = allStudents[index].getStudentID() + "," + allStudents[index].getName() + ","
-                            + allStudents[index].getCourse() + "," + allStudents[index].getGrade();
-                    fileWriter2.write(stCourse2 + "\n");
-                    course2[i2] = allStudents[index];
-                    i2++;
-                } else if (allStudents[index].getCourse().contains("APMTM")) {
-                    stCourse3 = allStudents[index].getStudentID() + "," + allStudents[index].getName() + ","
-                            + allStudents[index].getCourse() + "," + allStudents[index].getGrade();
-                    fileWriter3.write(stCourse3 + "\n");
-                    course2[i3] = allStudents[index];
-                    i3++;
-
-                }
+            for (int i=0; i< fileWriters.length; i++) {
+                headString = allStudents[0].getStudentID() + "," + allStudents[0].getName() + ","
+                        + allStudents[0].getCourse() + "," + allStudents[0].getGrade();
+                fileWriters[i].write(headString + "\n");
             }
+
+                for (int j=1; j< allStudents.length; j++) {
+
+                    Arrays.sort(allStudents, new Comparator<Student>() {
+
+                        @Override
+                        public int compare(Student o1, Student o2) {
+                            return o2.getGrade().compareTo(o1.getGrade());
+                        }
+                    });
+
+                    String courseStudents = allStudents[j].getStudentID() + "," + allStudents[j].getName() + ","
+                            + allStudents[j].getCourse() + "," + allStudents[j].getGrade();
+
+                    if (allStudents[j].getCourse().contains("COMPSCI")) {
+                        fileWriters[0].write(courseStudents + "\n");
+                    } else if (allStudents[j].getCourse().contains("STAT")) {
+                        fileWriters[1].write(courseStudents + "\n");
+                    } else {
+                        fileWriters[2].write(courseStudents + "\n");
+                }
+
+            }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                fileWriter.close();
+                for (int i=0; i< fileWriters.length; i++) {
+                    fileWriters[i].close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-//        Arrays.sort(course1, new Comparator <Student> () {
-//
-//            @Override
-//            public int compare(Student o1, Student o2) {
-//                return o1.getGrade().compareTo(o2.getGrade());
-//            }
-//        });
-
-        for (int j = 0; j < course1.length; j++) {
-            System.out.println(course1[j].getStudentID() + "," + course1[j].getName() + "," + course1[j].getCourse() + "," +
-                    course1[j].getGrade());
 
         }
-        for (int j = 0; j < course2.length; j++) {
-            System.out.println(course2[j].getStudentID() + "," + course2[j].getName() + "," + course2[j].getCourse() + "," +
-                    course2[j].getGrade());
-
-        }
-        for (int j = 0; j < course3.length; j++) {
-            System.out.println(course3[j].getStudentID() + "," + course3[j].getName() + "," + course3[j].getCourse() + "," +
-                    course3[j].getGrade());
-
-        }
-
 
     }
-
-
 
 }
